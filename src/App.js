cb78f0811e5252e887f836fd6ce0b8560a2ec28d
Long Pham <phamlong139@gmail.com>
2023-02-28 16:57:44 +0700

    Login GG

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {authentication} from "./firebase-config"
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 function App() {
+  const signInWithGoogle=() =>{
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(authentication, provider) .then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={signInWithGoogle}>
+          Click me
+        </button>
       </header>
     </div>
   );
